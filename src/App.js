801289082a758e4233d5b1307d2c0a8fff130a1e
Kemal Ahmed <goatandsheep@gmail.com>
2021-12-07 00:26:25 -0500

    gravity is working

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import './App.css';
 import "nes.css/css/nes.min.css";
 
-import { useEffect, useState, useRef
-  // useCallback
- } from "react";
+import { useEffect, useState, useRef } from "react";
 import { setup, isSupported } from "@loomhq/loom-sdk";
 // import { oembed } from "@loomhq/loom-embed";
 
@@ -37,24 +35,23 @@ function BlocksSection () {
     </div>)
 } 
 
+const gravMult = 5
 
 function ShowScore (props) {
   const diff = props.curr - props.start;
   const val = Math.floor(diff / 1000 || 0);
-  // console.log('tick', diff)
-  console.log('tick', props.gameTimerRef)
   return <span>{val}</span>
 }
 
 function App() {
   // const [videoHTML, setVideoHTML] = useState("");
-  const [vidButton, setVidButton] = useState({});
-  const [vidPos, setVidPos] = useState([0, 0]);
   const [showMessage, setShowMessage] = useState(false);
-  // const [timerOn, setTimerOn] = useState(false);
   const [startTime, setStartTime] = useState(NaN);
   const [currentTime, setCurrentTime] = useState(NaN);
   const gameTimerRef = useRef(null)
+  const playerSpeedRef = useRef(0)
+  const vidButtonRef = useRef({})
+  const vidPosRef = useRef([0, 0])
 
   window.onblur = () => {
     setShowMessage(true);
@@ -65,41 +62,48 @@ function App() {
   }
 
   const startWatch = () => {
-    console.log('start watch')
     const startTime = new Date().getTime();
     setStartTime(startTime);
 
     gameTimerRef.current = setInterval(() => {
+      // update timers
       const currTime = new Date().getTime();
       setCurrentTime(currTime);
 
-    }, 1000)
+      // gravity
+      // 9.8 m/s^2 * 0.05 s = 0.49 m/s
+      // multiplier
+      playerSpeedRef.current = playerSpeedRef.current + 0.49 * gravMult;
+      console.log('speed', playerSpeedRef.current)
+      vidPosRef.current[1] += playerSpeedRef.current;
+      // setVidPos([vidPos[0], vidPos[1] + playerSpeedRef.current]);
+      vidButtonRef.current.moveBubble({ x: vidPosRef.current[0], y: vidPosRef.current[1] + playerSpeedRef.current });
+    }, 50) // update every 50ms or 0.05s
   }
-
-  // const closeStartWatch = useCallback(startWatch, [gameTimerRef]);
 
   const stopWatchAct = () => {
     console.log('stop watch', gameTimerRef.current)
 
-    // setTimerOn(false);
     if (!gameTimerRef.current) {
       console.log('oh no')
     } else {
       clearInterval(gameTimerRef.current)
       gameTimerRef.current = null
+      playerSpeedRef.current = 0;
+      vidPosRef.current[0] = 0;
+      vidPosRef.current[1] = 0;
     }
-    // setTimerOn(false);
   }
-  // const stopWatch = useCallback(stopWatchAct, [gameTimerRef]);
 
   const handleMove = (e) => {
 
-    let newX = vidPos[0]
-    let newY = vidPos[1]
+    let newX = vidPosRef.current[0]
+    let newY = vidPosRef.current[1]
     if (e.key === "w" || e.key === "ArrowUp") {
-      newY = vidPos[1] - 10;
+      // newY = vidPosRef.current[1] - 10;
+      playerSpeedRef.current -= 10 * gravMult
     } else if (e.key === "s" || e.key === "ArrowDown") {
-      newY = vidPos[1] + 10;
+      newY = vidPosRef.current[1] + 10;
     // } else if (e.key === "a" || e.key === "ArrowLeft") {
     //   newX = vidPos[0] - 10;
     // } else if (e.key === "d" || e.key === "ArrowRight") {
@@ -109,13 +113,14 @@ function App() {
     }
 
     if (!(isNaN(newX) || isNaN(newY))) {
-      setVidPos([newX, newY]);
-      vidButton.moveBubble({ x: newX, y: newY });
+      vidPosRef.current = [newX, newY]
+      // setVidPos([newX, newY]);
+      vidButtonRef.current.moveBubble({ x: newX, y: newY });
       checkDead(newX, newY);
     } else {
       console.log('naan!')
     }
-    console.log(`${vidPos[0]}, ${vidPos[1]}`);
+    console.log(`${vidPosRef.current[0]}, ${vidPosRef.current[1]}`);
   }
 
   const checkDead = (x, y) => {
@@ -146,44 +151,18 @@ function App() {
    * lose!
    */
   const endGame = () => {
-    // setTimeout(() => {
-      try {
-        console.log('end game', vidButton)
-        vidButton.endRecording()
-        stopWatchAct()
-      } catch (e) {
-        console.error('error ending game', e)
-        console.log(endButton)
-      }
-    // }, 100)
-  }
-
-  /*
-  useEffect(() => {
-    if (timerOn) {
-      // startWatch();
-
-      const startTime = new Date().getTime();
-      setStartTime(startTime);
-      const timer = setInterval(() => {
-        const currTime = new Date().getTime();
-        setCurrentTime(currTime);
-      }, 1000)
-      console.log('timer', timer)
-      setGameTimerRef(timer);
-    } else {
-      stopWatch();
-
-      clearInterval(gameTimerRef)
-      // setGameTimerRef(null);
-      setTimerOn(false);
+    try {
+      // console.log('end game', vidButton)
+      vidButtonRef.current.endRecording()
+      stopWatchAct()
+    } catch (e) {
+      console.error('error ending game', e)
+      console.log(endButton)
     }
-  }, [timerOn])
-  */
+  }
 
   useEffect(() => {
     async function setupLoom() {
-      console.log('_______ setupLoom')
       const { supported, error } = await isSupported();
 
       if (!supported) {
@@ -207,9 +186,7 @@ function App() {
 
       const sdkButton = configureButton({ element: button });
 
-
-
-      setVidButton(sdkButton);
+      vidButtonRef.current = sdkButton;
 
       // sdkButton.on("insert-click", async video => {
       //   const { html } = await oembed(video.sharedUrl, { width: 400 });
@@ -222,7 +199,8 @@ function App() {
         const newX = vidPosit.x
         const newY = vidPosit.y
         if (!(isNaN(newX) || isNaN(newY))) {
-          setVidPos([vidPosit.x, vidPosit.y]);
+          vidPosRef.current = [vidPosit.x, vidPosit.y]
+          // setVidPos([vidPosit.x, vidPosit.y]);
         } else {
           console.log('naan!')
         }
@@ -278,10 +256,10 @@ function App() {
           <h3 className="title" style={{backgroundColor: '#3f96cf'}}>Accessibility</h3>
           <div>Height of next chimney: <span></span></div>
           <div>Time to next chimney: <span></span></div>
-          <div>Player height: <span>{(vidPos[1] * -1) + 'm'}</span></div>
+          <div>Player altitude: <span>{(Math.floor(vidPosRef.current[1] * -1)) + 'm'}</span></div>
         </section>
       </div>
-      <button id={BUTTON_ID} onKeyDown={handleMove} className="nes-btn is-success">Start Game</button>
+      <button id={BUTTON_ID} onKeyDown={handleMove} className="nes-btn is-success" style={{ opacity: gameTimerRef.current ? '0' : '1' }}>Start Game</button>
       {/* <div dangerouslySetInnerHTML={{ __html: videoHTML }}></div> */}
       <BlocksSection />
       <div style={{ bottom: 10, position: 'fixed', display: 'flex', justifyContent: 'space-between', alignItems: 'center', minWidth: '85%' }}>
